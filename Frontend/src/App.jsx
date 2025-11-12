bbd6c2af8fbb4060118ac5278e99ca1f7812a966
Jean <jeanfmve@gmail.com>
2025-11-12 16:14:58 -0300

    Se paso dev a mi rama

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,17 @@
-
+import React from "react"
+import AppRoutes from "./routes/main.routes.jsx"
+import {BrowserRouter} from 'react-router-dom'
 import {BusquedaIncidente } from './components/BusquedaIncidente'
 import HeroCarousel from './components/HeroCarousel'
 import {CardBicycleRack} from './components/CardBicycleRack'
 
 function App() {
-
   return (
     <>
     <div> 
-      
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
       <HeroCarousel />
       <BusquedaIncidente />
     </div>
