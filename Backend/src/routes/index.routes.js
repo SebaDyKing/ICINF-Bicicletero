653ccc45882145a9ef01ae3d71630cc83439cb2f
Jean <jeanfmve@gmail.com>
2025-11-12 15:39:16 -0300

    Se volvio el index.routes.js

---
+++ Backend/src/routes/index.routes.js
@@ -0,0 +1,16 @@
+"use scrict";
+// src/routes/index.routes.js
+import { Router } from "express";
+import guardRouter from "./guard.routes.js";
+import guardsAdmin from './guardsAdmin.routes.js'
+import guardsAdmin from './guardsAdmin.routes.js'
+
+export function routerApi(app) {
+  const router = Router();
+  app.use("/api", router);
+  //falta rutas auth
+  //router.use('/auth', authRoutes)
+  //Conecta el router de guardia a /api/guards
+  router.use("/guards", guardRouter);
+  router.use('/guardsAdmin', guardsAdmin)
+}
