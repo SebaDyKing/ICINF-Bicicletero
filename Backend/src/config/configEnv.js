372ccee4b03fd371ff2755db5f4caf6ab973f7e7
Jean <jeanfmve@gmail.com>
2025-11-11 22:17:14 -0300

    Se ordeno mi rama

--- Backend/src/config/configEnv.js
+++ Backend/src/config/configEnv.js
@@ -2,7 +2,7 @@ import {config} from 'dotenv'
 config()
 
 export const db = {
-    user: proccess.env.DB_USER,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
