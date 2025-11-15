c77da46cc8adbac148698839ab224b7bcd1c09a5
Jean <jeanfmve@gmail.com>
2025-11-15 20:01:30 -0300

    Se implemento en index.routes.js la ruta /auth, la cual permite utilizar la ruta /api/auth/login

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -4,7 +4,8 @@ import {loginUser/*, registerUser*/} from '../controllers/auth.controller.js'
 
 const router = Router()
 
-//router.post('/register', registerUser)
 router.post('/login', loginUser)
+//router.post('/register', registerUser)
+
 
 export default router
