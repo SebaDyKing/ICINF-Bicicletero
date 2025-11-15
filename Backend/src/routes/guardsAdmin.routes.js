51fcf5670959361d949c33197567f76b09120090
Jean <jeanfmve@gmail.com>
2025-11-15 17:13:32 -0300

    Se agregaron rutas de visualizar usuario especifico y todos los usuarios

--- Backend/src/routes/guardsAdmin.routes.js
+++ Backend/src/routes/guardsAdmin.routes.js
@@ -1,13 +1,16 @@
 import {Router} from 'express'
 import {createGuard, deleteGuard, updateGuard, getGuard, getAllGuards} from '../controllers/guardsAdmin.controller.js'
+import {getOwner, getAllOwners} from '../controllers/owner.controller.js'
 
 const router = Router()
 
-// /api/guardsAdmin/createGuard
+// /api/guardsAdmin/
 router.post('/createGuard', createGuard)
 router.put('/updateGuard', updateGuard)
 router.delete('/deleteGuard', deleteGuard)
 router.get('/getGuard', getGuard)
 router.get('/getAllGuards', getAllGuards)
+router.get('/getUser/:rut', getOwner)
+router.get('/getAllUsers', getAllOwners)
 
 export default router
