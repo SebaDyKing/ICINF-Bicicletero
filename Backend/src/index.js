4b0a0e312e85b1cd59367a28693525a30e0b1cef
Jean <jeanfmve@gmail.com>
2025-11-22 01:29:43 -0300

    Se agrega el uso de /uploads el cual seria la carpeta donde se guardarian las fotos de los reportes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,10 +6,19 @@ import { connectDB } from "./config/configDb.js";
 import { routerApi } from "./routes/index.routes.js";
 import { port } from "./config/configEnv.js"
 import { createCentral } from './config/initialSetup.js'
+import path from 'path'; //utilizar path para crear carpeta upload
+import { fileURLToPath } from 'url'; //tampoco se
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const uploadsPath = path.resolve(__dirname, '../../../uploads');
+
 app.use(express.json());
 app.use(morgan("dev"));
+app.use('/uploads', express.static(uploadsPath));
+
 connectDB()
   .then( async () => {
     await createCentral();
