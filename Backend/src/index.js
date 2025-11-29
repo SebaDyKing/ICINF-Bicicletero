65fe163cf6c44939f22fcf5b1f6e047ad9dbcb64
Jean <jeanfmve@gmail.com>
2025-11-29 14:55:15 -0300

    Se agrego el metodo PUT en los metodos que admite cors

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -24,11 +24,10 @@ const server = http.createServer(app);
 
 const corsOptions = {
   origin: ['http://localhost:5173','http://146.83.198.35:1354'],
-  methods: ['GET','POST','PATCH','DELETE'],
+  methods: ['GET','POST','PATCH','DELETE', 'PUT'],
   allowedHeaders: ['Content-Type','Authorization','Accept'],
   credentials : true
 };
-
 
 app.use(cors(corsOptions));
 app.use(express.json());
