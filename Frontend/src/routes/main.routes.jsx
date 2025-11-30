55ea16f080bb661c356c0165a25a644f276e6549
Neeidaan <neidanmartinez22@gmail.com>
2025-11-30 13:06:21 -0300

    Agrego la ruta para la página del guardia

--- Frontend/src/routes/main.routes.jsx
+++ Frontend/src/routes/main.routes.jsx
@@ -5,8 +5,9 @@ import LoginPage from '../features/Login/LoginPage';
 import RegisterPage from '../features/Login/RegisterPage';
 import { HomePage } from '../features/HomePage';
 import VerifyPage from '../features/Login/VerifyPage';
-import IncidentesPage from '../features/Guard/components/Incidentes';
+// import IncidentesPage from '../components/Incidentes';
 import SecurityDashboard from '../features/Central/components/SecurityDashboard';
+import GuardPages from '../features/Guard/GuardPages';
 
 const AppRoutes = () => {
     return (
@@ -20,8 +21,11 @@ const AppRoutes = () => {
               
             {/* Rutas Informes */} 
             <Route path='/infomain' element={<InfoMain />} /> 
-            <Route path='/incidentes' element={<IncidentesPage/>} /> 
+            {/* <Route path='/incidentes' element={<IncidentesPage/>} /> */} 
             <Route path='/central' element={<SecurityDashboard/>} />
+
+            {/* Rutas Guardia */}
+            <Route path='/guardia/home' element={<GuardPages />} />
 
             <Route path="*" element={<h1>404 - Página no encontrada</h1>} />
         </Routes>
