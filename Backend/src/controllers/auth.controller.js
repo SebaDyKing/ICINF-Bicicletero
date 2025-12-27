4128fb44c42a54fa19a30f215867525f0ea111ac
Jean <jeanfmve@gmail.com>
2025-12-27 06:09:54 -0300

    Se agrego un return en login

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -39,7 +39,7 @@ export const loginUser = async (req, res) => {
 
         //  3. Validar contraseña
         const isValidPass = await bcrypt.compare(contrasenia, userFound.contrasenia)
-        if(!isValidPass) handleErrorClient(res, 404, 'Contraseña incorrecta')
+        if(!isValidPass) return handleErrorClient(res, 404, 'Contraseña incorrecta')
 
         //  4. JWT - Guarda en un JWT todas las variables que tenga dentro del sign
         const token = jwt.sign({
