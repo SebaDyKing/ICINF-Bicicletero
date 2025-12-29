ec7cf0a1b138b36f4bfd109d334e84d8f81c64d4
Jean <jeanfmve@gmail.com>
2025-12-28 21:55:41 -0300

    Se agregaron regex en nombre, apellido, telefono

--- Backend/src/validations/guardia.validations.js
+++ Backend/src/validations/guardia.validations.js
@@ -16,24 +16,28 @@ export const guardBodyValition = Joi.object({
     'string.empty': `El campo rut no puede estar vacío.`,
     'string.pattern.base': `El formato del rut no es válido`,
   }),
-  nombre: Joi.string().min(3).required().messages({
+  nombre: Joi.string().min(3).pattern(/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/).required().messages({
     'string.base': `El campo nombre debe ser un texto.`,
     'string.min': `El campo nombre debe tener al menos 3 caracteres.`,
     'any.required': `El campo nombre es obligatorio.`,
+    'string.pattern.base': 'El campo nombre no puede contener números ni símbolos.',
     'string.empty': `El campo nombre no puede estar vacío.`,
   }),
 
-  apellido: Joi.string().min(3).required().messages({
+  apellido: Joi.string().min(3).pattern(/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/).required().messages({
     'string.base': `El campo apellido debe ser un texto.`,
     'any.required': `El campo apellido es obligatorio.`,
     'string.empty': `El campo apellido no puede estar vacío.`,
-    'string.min': `El campo apellido debe tener al menos 3 caracteres.`}),
+    'string.pattern.base': 'El campo apellido no puede contener números ni símbolos.',
+    'string.min': `El campo apellido debe tener al menos 3 caracteres.`
+  }),
 
-  telefono: Joi.string().min(15).max(15).required().messages({
+  telefono: Joi.string().min(15).max(15).required().pattern(/^\+56 9 \d{4} \d{4}$/).messages({
     'string.base': `El campo teléfono debe ser un texto.`,
     'string.min': `El campo teléfono se debe ingresar al menos 8 números.`,
     'string.max': `El campo teléfono no debe exceder los 15 caracteres.`,
     'any.required': `El campo teléfono es obligatorio.`,
+    'string.pattern.base': 'El teléfono debe tener el formato +56 9 XXXX XXXX y solo contener números.',
     'string.empty': `El campo teléfono no puede estar vacío.`,
   }),
   email: Joi.string().email().required().messages({
